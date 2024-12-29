adb8a74f82b668a487f5b793eba4451e4080ea22
Justudent <justudent09@mail.ru>
2024-12-30 00:43:17 +0300

    Update app.js

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ Telegram.WebApp.ready();
 Telegram.WebApp.onEvent('themeChanged', function() {
     document.documentElement.className = Telegram.WebApp.colorScheme;
 });
+
+Telegram.WebApp.MainButton.setParams({ 
+    text: 'Кнопка' 
+});
