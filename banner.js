1cb27d0112b8108b341212df8864828f4ef7cc88
Justudent <justudent09@mail.ru>
2024-12-30 18:52:04 +0300

    Update banner.js

--- banner.js
+++ banner.js
@@ -1,5 +1,7 @@
 Telegram.WebApp.ready();
 
+Telegram.WebApp.MainButton.hide();
+
 Telegram.WebApp.onEvent('themeChanged', function() {
     document.documentElement.className = Telegram.WebApp.colorScheme;
 });
