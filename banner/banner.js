1552d4231c2cff2c6808573f7e64c570f46c547b
Justudent <justudent09@mail.ru>
2024-12-30 17:51:14 +0300

    Update banner.js

---
+++ banner/banner.js
@@ -0,0 +1,23 @@
+Telegram.WebApp.ready();
+
+Telegram.WebApp.onEvent('themeChanged', function() {
+    document.documentElement.className = Telegram.WebApp.colorScheme;
+});
+
+Telegram.WebApp.MainButton.setParams({ 
+    text: 'Main Button' 
+});
+
+Telegram.WebApp.MainButton.onClick(function () {
+    Telegram.WebApp.showAlert('Main Button was clicked')
+});        
+
+Telegram.WebApp.MainButton.show();
+
+function toggleMainButton() {
+    if (Telegram.WebApp.MainButton.isVisible) {
+        Telegram.WebApp.MainButton.hide();
+    } else {
+        Telegram.WebApp.MainButton.show();
+    }
+};
