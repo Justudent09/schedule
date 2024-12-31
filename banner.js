6d24d58eebed621586f81c849629d036c87efca5
Justudent <justudent09@mail.ru>
2024-12-31 15:29:41 +0300

    Update banner.js

--- banner.js
+++ banner.js
@@ -1,20 +1,17 @@
-// Инициализация Telegram WebApp
 Telegram.WebApp.ready();
 Telegram.WebApp.MainButton.hide();
 
-// Смена темы в зависимости от Telegram WebApp
 Telegram.WebApp.onEvent('themeChanged', function() {
     document.documentElement.className = Telegram.WebApp.colorScheme;
 });
 
-// Загрузка Lottie-анимации
 document.addEventListener('DOMContentLoaded', () => {
-    fetch('DuckEmoji.json') // Локальный JSON-файл
+    fetch('DuckEmoji.json')
         .then(response => {
             if (!response.ok) {
                 throw new Error('Ошибка при загрузке файла: ' + response.statusText);
             }
-            return response.json(); // Преобразуем ответ в JSON
+            return response.json(); 
         })
         .then(animationData => {
             lottie.loadAnimation({
@@ -22,7 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 renderer: 'svg',
                 loop: true,
                 autoplay: true,
-                animationData: animationData // Используем JSON-данные
+                animationData: animationData 
             });
         })
         .catch(error => {
