3ea4d802b3f92f50c1b433c787eebebcec38413d
Justudent <justudent09@mail.ru>
2024-12-31 13:10:52 +0300

    Update banner.js

--- banner.js
+++ banner.js
@@ -6,4 +6,11 @@ Telegram.WebApp.onEvent('themeChanged', function() {
     document.documentElement.className = Telegram.WebApp.colorScheme;
 });
 
+lottie.loadAnimation({
+    container: document.getElementById('animation-container'),
+    renderer: 'svg',
+    loop: true, 
+    autoplay: true,
+    path: 'DuckEmoji.tgs'
+});
 
