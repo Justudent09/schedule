6b4402cee2aba2cee750f9f79da6c7bbb809a10f
Justudent <justudent09@mail.ru>
2025-01-01 21:01:28 +0300

    Update

---
+++ auth.js
@@ -0,0 +1,60 @@
+// auth.js
+
+function initializeAuthAnimation() {
+    const container = document.getElementById('animation-container');
+    if (container) {
+        container.innerHTML = '';
+        fetch('DuckEmojiTeacher.json', { cache: 'reload' })
+            .then(response => response.json())
+            .then(animationData => {
+                lottie.loadAnimation({
+                    container: container,
+                    renderer: 'svg',
+                    loop: true,
+                    autoplay: true,
+                    animationData: animationData
+                });
+            })
+            .catch(error => console.error('Ошибка при загрузке анимации:', error));
+    }
+}
+
+// Прокрутка и точки
+function initializeAuthScroll() {
+    const scrollContainer = document.getElementById('horizontal-scroll');
+    if (scrollContainer) {
+        scrollContainer.addEventListener('scroll', handleAuthScroll);
+    }
+}
+
+function handleAuthScroll() {
+    const scrollContainer = document.getElementById('horizontal-scroll');
+    const scrollItems = document.querySelectorAll('.scroll-item');
+    const buttons = document.querySelectorAll('#app .button');
+
+    if (scrollContainer) {
+        const containerWidth = scrollContainer.offsetWidth;
+        const scrollLeft = scrollContainer.scrollLeft;
+        const index = Math.round(scrollLeft / containerWidth);
+
+        scrollItems.forEach((item, i) => item.classList.toggle('active', i === index));
+        buttons.forEach((button, i) => button.classList.toggle('active', i === index));
+    }
+}
+
+function unloadAuthAnimation() {
+    const scrollContainer = document.getElementById('horizontal-scroll');
+    if (scrollContainer) {
+        scrollContainer.removeEventListener('scroll', handleAuthScroll);
+    }
+    const container = document.getElementById('animation-container');
+    if (container) {
+        container.innerHTML = '';
+    }
+}
+
+function initAuth() {
+    console.log('Инициализация auth.js');
+    initializeAuthAnimation();
+    initializeAuthScroll();
+}
