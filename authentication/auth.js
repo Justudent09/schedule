0a4a75d3e8838bc8a0c54586913ff4796b7dcc84
Justudent <justudent09@mail.ru>
2025-01-01 16:42:15 +0300

    Update auth.js

---
+++ authentication/auth.js
@@ -0,0 +1,48 @@
+Telegram.WebApp.ready();
+Telegram.WebApp.MainButton.hide();
+
+Telegram.WebApp.onEvent('themeChanged', function() {
+    document.documentElement.className = Telegram.WebApp.colorScheme;
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('DuckEmojiTeacher.json')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Ошибка при загрузке файла: ' + response.statusText);
+            }
+            return response.json(); 
+        })
+        .then(animationData => {
+            lottie.loadAnimation({
+                container: document.getElementById('animation-container'),
+                renderer: 'svg',
+                loop: true,
+                autoplay: true,
+                animationData: animationData 
+            });
+        })
+        .catch(error => {
+            console.error('Ошибка при загрузке анимации:', error);
+        });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const scrollContainer = document.getElementById('horizontal-scroll');
+    const scrollItems = document.querySelectorAll('.scroll-item');
+    const buttons = document.querySelectorAll('#app .button');
+
+    scrollContainer.addEventListener('scroll', () => {
+        const containerWidth = scrollContainer.offsetWidth;
+        const scrollLeft = scrollContainer.scrollLeft;
+        const index = Math.round(scrollLeft / containerWidth);
+
+        scrollItems.forEach((item, i) => {
+            item.classList.toggle('active', i === index);
+        });
+
+        buttons.forEach((button, i) => {
+            button.classList.toggle('active', i === index);
+        });
+    });
+});
