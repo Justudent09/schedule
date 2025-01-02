189243d50168d0e5dbc10636931ae6d3f03fca2d
Justudent <justudent09@mail.ru>
2025-01-02 12:28:55 +0300

    Update

---
+++ schedule.js
@@ -0,0 +1,8 @@
+function initShedule() {
+    console.log('Schedule page initialized');
+}
+
+// Опционально: добавьте очистку перед переходом
+function unloadShedule() {
+    console.log('Schedule page unloaded');
+}
