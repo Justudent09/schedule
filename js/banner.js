0e33fd5e4cac4cc844385ca9ff413e852b898513
Justudent <justudent09@mail.ru>
2025-01-03 17:26:48 +0300

    Update banner.js

--- js/banner.js
+++ js/banner.js
@@ -2,16 +2,6 @@ if (window.Telegram && Telegram.WebApp) {
     Telegram.WebApp.expand();
 }
 
-function navigateToPage(url) {
-            if (document.startViewTransition) {
-                document.startViewTransition(() => {
-                    window.location.href = url;
-                });
-            } else {
-                window.location.href = url; // Fallback для старых браузеров
-            }
-        }
-
 const animationContainer = document.getElementById('animation-container');
 
 if (animationContainer) {
@@ -56,3 +46,12 @@ if (scrollContainer) {
 }
 
 
+const joinButton = document.getElementById('joinButton');
+
+if (joinButton) {
+    joinButton.addEventListener('click', () => {
+        window.location.href = 'auth.html';
+    });
+} else {
+    Telegram.WebApp.showAlert('⚠️ Кнопка перехода не найдена');
+}
