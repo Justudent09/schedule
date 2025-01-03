358e55b2bd5b1545b059b49fca102da7310c3008
Justudent <justudent09@mail.ru>
2025-01-03 23:25:39 +0300

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -5,7 +5,7 @@ if (window.Telegram && Telegram.WebApp) {
 function selectOption(selected) {
     document.querySelectorAll('.option').forEach(option => {
         option.classList.remove('selected');
-});
+    });
 
     selected.classList.add('selected');
 }
