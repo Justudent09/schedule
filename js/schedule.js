39baca3edb64700a1df0a8d96ec1271eecd0e7ca
Justudent <justudent09@mail.ru>
2025-01-06 14:23:22 +0300

    Update schedule.js

--- js/schedule.js
+++ js/schedule.js
@@ -1,3 +1,31 @@
 if (window.Telegram && Telegram.WebApp) {
     Telegram.WebApp.expand();
 }
+
+function showSavedRole() {
+    const key = 'userRole';
+
+    Telegram.WebApp.CloudStorage.getItem(key, (error, value) => {
+        if (error) {
+            Telegram.WebApp.showAlert('Ошибка при получении данных: ' + error);
+        } else if (value) {
+            Telegram.WebApp.showAlert('Сохранённое значение: ' + value);
+        } else {
+            Telegram.WebApp.showAlert('Значение не найдено');
+        }
+    });
+}
+
+function showSavedYear() {
+    const key = 'userYear';
+
+    Telegram.WebApp.CloudStorage.getItem(key, (error, value) => {
+        if (error) {
+            Telegram.WebApp.showAlert('Ошибка при получении данных: ' + error);
+        } else if (value) {
+            Telegram.WebApp.showAlert('Сохранённое значение: ' + value);
+        } else {
+            Telegram.WebApp.showAlert('Значение не найдено');
+        }
+    });
+}
