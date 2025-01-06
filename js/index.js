191bc599cd232c61b76b60f01532efcf2d78c3b5
Justudent <justudent09@mail.ru>
2025-01-06 21:48:07 +0300

    Update js

---
+++ js/index.js
@@ -0,0 +1,41 @@
+function showSavedRole() {
+    const key = 'userRole';
+
+    Telegram.WebApp.CloudStorage.getItem(key, (error, value) => {
+        if (error) {
+            Telegram.WebApp.showAlert('Ошибка при получении данных: ' + error);
+        } else if (value) {
+            Telegram.WebApp.showAlert('Сохранённое значение: ' + value);
+        } else {
+            Telegram.WebApp.showAlert('Значение не найдено');
+        }
+    });
+}
+
+function showSavedYear() {
+    const key = 'userYear';
+
+    Telegram.WebApp.CloudStorage.getItem(key, (error, value) => {
+        if (error) {
+            Telegram.WebApp.showAlert('Ошибка при получении данных: ' + error);
+        } else if (value) {
+            Telegram.WebApp.showAlert('Сохранённое значение: ' + value);
+        } else {
+            Telegram.WebApp.showAlert('Значение не найдено');
+        }
+    });
+}
+
+function showSavedDirection() {
+    const key = 'userDirection';
+
+    Telegram.WebApp.CloudStorage.getItem(key, (error, value) => {
+        if (error) {
+            Telegram.WebApp.showAlert('Ошибка при получении данных: ' + error);
+        } else if (value) {
+            Telegram.WebApp.showAlert('Сохранённое значение: ' + value);
+        } else {
+            Telegram.WebApp.showAlert('Значение не найдено');
+        }
+    });
+}
