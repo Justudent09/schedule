fc8dd1fb67b8cd3abbdbda1fed246424a8a07a86
Justudent <justudent09@mail.ru>
2025-01-21 15:14:47 +0300

    Update studentDirection.js

--- js/studentDirection.js
+++ js/studentDirection.js
@@ -1,19 +1,6 @@
 if (window.Telegram && Telegram.WebApp) {
     Telegram.WebApp.expand();
     Telegram.WebApp.lockOrientation();
-}
-
-const inactiveSVG = `
-<svg width="20" height="20" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <circle cx="10" cy="10" r="9" fill="none" stroke="var(--tg-theme-hint-color)" stroke-width="2"/>
-</svg>`;
-
-const activeSVG = `
-<svg width="20" height="20" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <circle cx="10" cy="10" r="9" fill="none" stroke="var(--tg-theme-accent-text-color)" stroke-width="2"/>
-    <circle cx="10" cy="10" r="5" fill="var(--tg-theme-accent-text-color)"/>
-</svg>`;
-
 document.querySelectorAll('.option').forEach(option => {
     option.querySelector('.icon').innerHTML = inactiveSVG;
     option.classList.remove('selected');
@@ -34,6 +21,20 @@ Telegram.WebApp.CloudStorage.getItem('userYear', (error, value) => {
             .forEach(option => option.classList.add('disabled'));
     }
 });
+}
+
+const inactiveSVG = `
+<svg width="20" height="20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="10" cy="10" r="9" fill="none" stroke="var(--tg-theme-hint-color)" stroke-width="2"/>
+</svg>`;
+
+const activeSVG = `
+<svg width="20" height="20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="10" cy="10" r="9" fill="none" stroke="var(--tg-theme-accent-text-color)" stroke-width="2"/>
+    <circle cx="10" cy="10" r="5" fill="var(--tg-theme-accent-text-color)"/>
+</svg>`;
+
+
 
 function selectOption(selected) {
     if (selected.classList.contains('disabled')) {
