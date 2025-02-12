05871a07099c612fb6357af986dcf50ae3601233
Justudent <justudent09@mail.ru>
2025-02-12 16:11:03 +0300

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -6,7 +6,7 @@ if (window.Telegram && Telegram.WebApp) {
 const animationContainer = document.getElementById('animation-container');
 
 if (animationContainer) {
-    fetch('assets/DuckEmojiWay.json', { cache: 'reload' })
+    fetch('assets/DuckEmojiWay.json', { cache: 'default' })
         .then(response => {
             if (!response.ok) {
                 throw new Error(`HTTP error! Status: ${response.status}`);
