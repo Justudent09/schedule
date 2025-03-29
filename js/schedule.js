94c2b570227f8395321bd15764bcbadd210f740b
Justudent <justudent09@mail.ru>
2025-03-29 13:13:47 +0300

    Update schedule.js

--- js/schedule.js
+++ js/schedule.js
@@ -3,25 +3,7 @@ if (window.Telegram && Telegram.WebApp) {
     Telegram.WebApp.lockOrientation();
 }
 
-function deleteItem() {
-    Telegram.WebApp.showConfirm("Выйти из аккаунта?", function (confirmed) {
-        if (confirmed) {
-            const keys = ['userRole', 'userYear', 'userDirection'];
-
-            keys.forEach(key => {
-                Telegram.WebApp.CloudStorage.removeItem(key, (error, success) => {
-                    if (error) {
-                        Telegram.WebApp.showAlert('Ошибка удаления: ' + error);
-                    }
-                });
-            });
-
-            linkButton();
-        } else {
-        }
-    });
-}
-
-function linkButton() {
-    window.location.href = 'banner.html';
+Telegram.WebApp.onEvent('settingsButtonClicked', function() { 
+        window.location.href = 'setting.html'; 
+    }); 
 }
