440e703cd8d1ba8488391caa0ed6effa3f1ddc20
Justudent <justudent09@mail.ru>
2025-03-29 16:46:43 +0300

    Update schedule.js

--- js/schedule.js
+++ js/schedule.js
@@ -4,6 +4,10 @@ if (window.Telegram && Telegram.WebApp) {
     Telegram.WebApp.SettingsButton.show();
 }
 
+Telegram.WebApp.SettingsButton.onClick(function(){ 
+    window.location.href = 'setting.html'; 
+});
+
 function deleteItem() {
     Telegram.WebApp.showConfirm("Выйти из аккаунта?", function (confirmed) {
         if (confirmed) {
