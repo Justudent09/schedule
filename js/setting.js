89d1ae05cd7e023c01c5251c729587266b02e2bd
Justudent <justudent09@mail.ru>
2025-03-29 13:16:45 +0300

    Create setting.js

---
+++ js/setting.js
@@ -0,0 +1,33 @@
+if (window.Telegram && Telegram.WebApp) {
+    Telegram.WebApp.expand();
+    Telegram.WebApp.lockOrientation();
+    Telegram.WebApp.BackButton.show();
+}
+
+Telegram.WebApp.onEvent('backButtonClicked', function() { 
+        window.location.href = 'schedule.html';       
+    });
+}
+
+function deleteItem() {
+    Telegram.WebApp.showConfirm("Выйти из аккаунта?", function (confirmed) {
+        if (confirmed) {
+            const keys = ['userRole', 'userYear', 'userDirection'];
+
+            keys.forEach(key => {
+                Telegram.WebApp.CloudStorage.removeItem(key, (error, success) => {
+                    if (error) {
+                        Telegram.WebApp.showAlert('Ошибка удаления: ' + error);
+                    }
+                });
+            });
+
+            linkButton();
+        } else {
+        }
+    });
+}
+
+function linkButton() {
+    window.location.href = 'banner.html';
+}
