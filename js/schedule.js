1691a68273104e83fd3acfe02fbfabc3d980b369
Justudent <justudent09@mail.ru>
2025-03-29 16:00:58 +0300

    Update schedule.js

--- js/schedule.js
+++ js/schedule.js
@@ -2,10 +2,7 @@ if (window.Telegram && Telegram.WebApp) {
     Telegram.WebApp.expand();
     Telegram.WebApp.lockOrientation();
     Telegram.WebApp.SettingsButton.show();
-    Telegram.WebApp.SettingsButton.onClick(function(){ 
-        window.location.href = 'setting.html'; 
-    });
-}
+    
 }
 
 function deleteItem() {
