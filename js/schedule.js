35ff658f5b4d214fb032cd116ffcc2a636ef23e6
Justudent <justudent09@mail.ru>
2025-03-30 15:19:23 +0300

    Update schedule.js

--- js/schedule.js
+++ js/schedule.js
@@ -2,6 +2,7 @@ if (window.Telegram && Telegram.WebApp) {
     Telegram.WebApp.expand();
     Telegram.WebApp.lockOrientation();
     Telegram.WebApp.SettingsButton.show();
+    Telegram.WebApp.BackButton.hide();
 }
 
 Telegram.WebApp.SettingsButton.onClick(function(){ 
