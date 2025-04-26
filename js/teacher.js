9217d2d2ee0f9111fc0365950309b77feed9957f
Justudent <justudent09@mail.ru>
2025-04-26 12:55:23 +0300

    Update teacher.js

--- js/teacher.js
+++ js/teacher.js
@@ -2,8 +2,12 @@ if (window.Telegram && Telegram.WebApp) {
     Telegram.WebApp.expand();
     Telegram.WebApp.lockOrientation();
     Telegram.WebApp.SettingsButton.hide();
-    Telegram.WebApp.BackButton.hide();
+    Telegram.WebApp.BackButton.show();
 }
+
+Telegram.WebApp.BackButton.onClick(function(){ 
+    window.location.href = 'auth.html'; 
+});
 
 const animationContainer = document.getElementById('animation-container');
 
