210c0466a2704323379c7ead7e86fec62f5d7b82
Justudent <justudent09@mail.ru>
2025-05-30 15:32:39 +0300

    Update schedule.js

--- js/schedule.js
+++ js/schedule.js
@@ -8,3 +8,35 @@ if (window.Telegram && Telegram.WebApp) {
 Telegram.WebApp.SettingsButton.onClick(function(){ 
     window.location.href = 'setting.html'; 
 });
+
+const animations = [
+    'assets/DuckEmojiPodcast.json',
+    'assets/DuckEmojiGaming.json'
+];
+    
+animations.forEach((animationUrl, index) => {
+    const containerId = `animation-container-${index + 1}`;
+    const animationContainer = document.getElementById(containerId);
+      
+    if (animationContainer) {
+        fetch(animationUrl, { cache: 'default' })
+            .then(response => {
+                if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+                return response.json();
+            })
+            .then(animationData => {
+                lottie.loadAnimation({
+                    container: animationContainer,
+                    renderer: 'svg',
+                    loop: true,
+                    autoplay: true,
+                    animationData: animationData
+                });
+            })
+        .catch(error => {
+            console.error(`Ошибка загрузки анимации ${index + 1}:`, error);
+        });
+    } else {
+        console.warn(`Контейнер ${containerId} не найден`);
+    }
+});
