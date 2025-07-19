e9ce0426349d6a7ed6541608c41a31b4385324a2
Justudent <justudent09@mail.ru>
2025-07-19 20:50:20 +0300

    Update studentDirection.js

--- js/studentDirection.js
+++ js/studentDirection.js
@@ -10,14 +10,14 @@ Telegram.WebApp.BackButton.onClick(function(){
 });
 
 const inactiveSVG = `
-<svg width="20" height="20" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <circle cx="10" cy="10" r="9" fill="none" stroke="var(--tg-theme-hint-color)" stroke-width="2"/>
+<svg width="6vw" height="6vw" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="3vw" cy="3vw" r="2.7vw" fill="none" stroke="var(--tg-theme-hint-color)" stroke-width="0.6vw"/>
 </svg>`;
 
 const activeSVG = `
-<svg width="20" height="20" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <circle cx="10" cy="10" r="9" fill="none" stroke="var(--tg-theme-accent-text-color)" stroke-width="2"/>
-    <circle cx="10" cy="10" r="5" fill="var(--tg-theme-accent-text-color)"/>
+<svg width="6vw" height="6vw" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="3vw" cy="3vw" r="2.7vw" fill="none" stroke="var(--tg-theme-accent-text-color)" stroke-width="0.6vw"/>
+    <circle cx="3vw" cy="3vw" r="1.5vw" fill="var(--tg-theme-accent-text-color)"/>
 </svg>`;
 
 document.querySelectorAll('.option').forEach(option => {
