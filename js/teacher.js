2cc675249c38c3be72b99bb929007bf68db7bd0e
Justudent <justudent09@mail.ru>
2025-07-20 23:09:27 +0300

    Update teacher.js

--- js/teacher.js
+++ js/teacher.js
@@ -9,25 +9,10 @@ Telegram.WebApp.BackButton.onClick(function(){
     window.location.href = 'auth.html'; 
 });
 
-const animationContainer = document.getElementById('animation-container');
-
-if (animationContainer) {
-    fetch('assets/DuckEmojiDeadend.json', { cache: 'default' })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-            return response.json();
-        })
-        .then(animationData => {
-            lottie.loadAnimation({
-                container: animationContainer,
-                renderer: 'svg',
-                loop: true,
-                autoplay: true,
-                animationData: animationData
-            });
-        })
-} else {
-    Telegram.WebApp.showAlert('⚠️ Контейнер анимации не найден');
-}
+lottie.loadAnimation({
+    container: document.getElementById('animation-container'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'assets/DuckEmojiDeadend.json'
+});
